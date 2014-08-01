15b9817d8b25b04bbedc9ac07318ecb61f058559
Menelle <thomasmenelle@gmail.com>
2014-08-01 15:40:28 +0200

    first commit

--- app/controllers/ig_controller.rb
+++ app/controllers/ig_controller.rb
@@ -1,8 +1,8 @@
 class IgController < ApplicationController
 	after_filter :allow_iframe
 	def index
-		@user = Instagram.user("1050190116")
-  	@feed = Instagram.user_recent_media("1050190116", {count: 60})
+		@user = Instagram.user("1445963564")
+  	@feed = Instagram.user_recent_media("1445963564", {count: 60})
   end
 	
   private
@@ -10,3 +10,7 @@ class IgController < ApplicationController
 	    response.headers.except! 'X-Frame-Options'
 	  end 
 end
+
+# Client ID 	4476e229961f41c28929b8f20745e70a
+# Client Secret 	5cc4ab509123469db2eeecb1126e6f50
+# http://luminaled-fb.herokuapp.com/ig/index
